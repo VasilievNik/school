c1d75225159c750dcc4759f48735d6a92bdf338a
nikit <nikita.veselyev@gmail.com>
2022-10-27 13:18:21 +0300

    15th commit

--- src/main/resources/liquibase/scripts/lesson-three.sql
+++ src/main/resources/liquibase/scripts/lesson-three.sql
@@ -1,11 +1,8 @@
-- liquibase formated sql
+--liquibase formatted sql
 
-- changeset nikita:1
+--changeset nikita:1
 CREATE INDEX find_student_by_name ON student (name);
-SELECT * FROM student WHERE name ='Gneg'
 
-- changeset nikita:2
+--changeset nikita:2
 CREATE INDEX find_faculty_by_name_and_color ON faculty (name, color);
-SELECT * FROM student
-WHERE name ='Slizz' AND name ='Green'
 
